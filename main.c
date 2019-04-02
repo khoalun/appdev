44c711603f15a099b0f8eefb3aaaff36014a469d
khoabui <buikhoa793@gmail.com>
2019-04-02 08:56:29 +0100

    main.c modified

--- main.c
+++ main.c
@@ -1,16 +1,35 @@
-#include<stdlib.h>		// for random numbers
-#include<time.h>		//for randomization (using system time)
+#include <stdlib.h> // for random numbers
 #include "screen.h"
-#include<stdio.h>
-int main() {
-	int dec[COL], i;		// 80-pieces of sound decibels
-	srand(time(NULL));
-	for(i=0; i<COL; i++) dec[i]= rand()%70+30;
-	clearScreen();
-	setColors(RED, bg(YELLOW));
-	barChart(dec);
-	//printf("Message from main\n");
-	resetColors();
-//	getchar();
-	//printf("Message from main\n");
+#include <time.h> // for randomization (using system time)
+#include <stdio.h>
+#include "sound.h"
+
+int main(){
+    FILE *f;
+	short sd[8000];
+	for(;;){
+		system("arecord -r16000 -c1 -f S16_LE -d5 test.wav");
+		f = fopen("test.wav","r");
+    	if (f == NULL){
+        printf("Cannot open the file\n");
+        return 1;
+    }
+//  int dec[COL]; // 80 pieces of sound decibels
+//  srand(time(NULL));
+//  for (int i=0; i<COL; i++) dec[i]=rand()%70+30;
+    clearScreen();
+    setColors(RED, bg(YELLOW));
+//  barChart(dec);
+    struct WAVHDR hdr;
+    fread(&hdr, sizeof(hdr), 1, f); // read WAV header
+	fread(&sd, sizeof(sd), 1, f);	// read WAV data
+	fclose(f);
+    displayWAVHDR(hdr);
+	// display WAVDATA()
 }
+    resetColors();
+    getchar();
+
+}
+
+
